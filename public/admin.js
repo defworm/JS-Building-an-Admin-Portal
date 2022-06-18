4d816c0f591befd42dc3d83245c088abe6655171
Crystal Korth <crystalkorth@gmail.com>
2022-06-18 14:30:03 -0500

    working in class

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,5 @@
 async function admin(){
-// tretrieve a list of books from server
+// retrieve a list of books from server
 const bookListResponse = await fetch ( 'http://localhost.3001/listBooks');
 const bookList = await bookListResponse.json();
 
@@ -15,6 +15,30 @@ bookList.forEach(book => {
     const li = document.createElement('li')
     li.textContent = book.title;
 
+    const textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.value = book.quantity;
+    li.append(textInput);
+
+    const button = document.createElement('input');
+    button.type = 'button';
+    button.value = 'Save';
+    button.addEventListener('click', async() => {
+        await fetch('http://localhost:2001/updateBook', {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: textInput.value,
+            }
+
+            )
+        })
+    })
+
+    li.append(button);
     ul.append(li);
 
 });
